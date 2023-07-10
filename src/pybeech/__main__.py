2e04708e610f38127f3e0fab8553f6178987fffe
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-07-10 03:04:00 +0100

    Add test code for Lexer

--- src/pybeech/__main__.py
+++ src/pybeech/__main__.py
@@ -1,1 +1,8 @@
 """Main script for pybeech package."""
+
+from src.pybeech.lexer import Lexer
+
+source = "{'hello' 0}"
+lexer = Lexer(source)
+
+print(*(f"{token.type = }, {token.value() = }\n" for token in lexer))

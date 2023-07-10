6bb4cd713bf8756c96edbbcb483d00f289430519
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-07-10 17:30:18 +0100

    Update __main__.py

--- src/pybeech/__main__.py
+++ src/pybeech/__main__.py
@@ -2,7 +2,8 @@
 
 from src.pybeech.lexer import Lexer
 
-source = "{'hello' 0 \"there\\nhi\n'where\\\"'}"
+source = "{'hello' 0 \"there\\nhi\n'where\\\"'  # This is a line comment\n"\
+         "~{This is a ~{nested}~ block comment}~} ~not-a-comment symbol~{comment}~"
 lexer = Lexer(source)
 
 print(*lexer, sep="\n")

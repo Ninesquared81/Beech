007ba5be4cac163e112731fde03fdb175c65b33d
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-07-21 05:01:25 +0100

    Start implementation of evaluator

---
+++ src/pybeech/evaluator.py
@@ -0,0 +1,11 @@
+"""Evaluate a Beech tree"""
+
+from .beech_types import Tree
+
+
+class Evaluator:
+    def __init__(self, tree: Tree):
+        self._tree = tree
+
+    def evaluate(self):
+        pass

cd2bc226d21f74684918cd7f289a632d6a77ded0
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-08-07 21:09:27 +0100

    Use raw `dict` and `list` for `Tree` and `List` types

--- src/pybeech/beech_types.py
+++ src/pybeech/beech_types.py
@@ -15,50 +15,6 @@ class Key:
         return hash(self.token.value)
 
 
-class Tree:
-    def __init__(self) -> None:
-        self._comments: list[Token] = []
-        self._data: dict[Key, Value] = {}
-
-    def add_comment(self, comment: Token) -> None:
-        self._comments.append(comment)
-
-    def insert_kv_pair(self, key: Key, value: Value) -> None:
-        self._data[key] = value
-
-    @property
-    def data(self) -> dict[Key, Value]:
-        return self._data.copy()
-
-    @property
-    def comments(self) -> list[Token]:
-        return self._comments[:]
-
-    def __repr__(self) -> str:
-        return f"<Tree: comments={self.comments}, data={self.data}>"
-
-
-class List:
-    def __init__(self) -> None:
-        self._comments: list[Token] = []
-        self._data: list[Value] = []
-
-    def add_comment(self, comment: Token):
-        self._comments.append(comment)
-
-    def add_value(self, value: Value):
-        self._data.append(value)
-
-    @property
-    def data(self) -> list[Value]:
-        return self._data[:]
-
-    @property
-    def comments(self) -> list[Token]:
-        return self._comments[:]
-
-    def __repr__(self) -> str:
-        return f"<List: comments={self._comments}, data={self._data}>"
-
-
-Value = typing.Union[str, Tree, List]
+Value = typing.Union[str, 'Tree', 'List']
+Tree = dict[Key, Value]
+List = list[Value]

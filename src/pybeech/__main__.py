fe8d60482fee0e447078abbcb384a96b74e71b21
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-08-10 03:11:22 +0100

    Try out symbol_parser.py in __main__.py

--- src/pybeech/__main__.py
+++ src/pybeech/__main__.py
@@ -2,6 +2,7 @@
 from typing import Union
 
 from src.pybeech.beech_types import Symbol
+from src.pybeech.default_mode.symbol_parser import parse_symbol
 from src.pybeech.lexer import Lexer
 from src.pybeech.parser import Parser
 from src.pybeech.transformer import transformer
@@ -36,21 +37,21 @@ tree = parser.parse()
 print(tree)
 
 
-class NumberSymbol(Symbol):
-    def __init__(self, number: int):
-        super().__init__(str(int))
-        self._number = number
+# class NumberSymbol(Symbol):
+#     def __init__(self, number: int):
+#         super().__init__(str(int))
+#         self._number = number
+#
+#     def __repr__(self) -> str:
+#         return f"NumberSymbol({self._number})"
+#
+#
+# def transform_symbol(symbol: Symbol) -> Union[NumberSymbol, Symbol]:
+#     try:
+#         return NumberSymbol(int(str(symbol)))
+#     except ValueError:
+#         return symbol
+#
 
-    def __repr__(self) -> str:
-        return f"NumberSymbol({self._number})"
-
-
-def transform_symbol(symbol: Symbol) -> Union[NumberSymbol, Symbol]:
-    try:
-        return NumberSymbol(int(str(symbol)))
-    except ValueError:
-        return symbol
-
-
-trans = transformer(transform_symbol=transform_symbol)
+trans = transformer(transform_symbol=parse_symbol)
 print(trans(tree))
